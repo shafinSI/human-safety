70f89f92a2f938c0977fbf666029aa3adc8b85c0
Al Imran <150480569+imran0279@users.noreply.github.com>
2026-05-20 13:21:09 +0600

    Adjust PrismaClient logging for development mode
    Modify PrismaClient logging based on environment.

--- app/Backend/lib/prisma.ts
+++ app/Backend/lib/prisma.ts
@@ -6,6 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ log: ['query'] })
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
